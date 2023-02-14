367dbea370e65bcd4379347b1c35240ee269acd2
tasbiha11 <tasbihahussain@yahoo.com>
2023-02-14 21:29:02 +0600

    first commit

---
+++ Problem1/index.js
@@ -0,0 +1,6 @@
+/*
+Write a JavaScript program to check two numbers and return true if one of the number is 100 or if the sum of the two number is 100
+*/
+
+const isEqualTo = (x, y) => x === 100 || y === 100 || (x + y) === 100;
+
